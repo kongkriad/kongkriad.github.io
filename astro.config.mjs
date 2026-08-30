dd386a35b5192d59ee4d8969a98cd5d407d46491
kong <tee0918831415@gmail.com>
2026-08-30 16:01:38 +0700

    add tailwind and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kongkriad.github.io'
+  site: 'https://kongkriad.github.io',
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
 });
